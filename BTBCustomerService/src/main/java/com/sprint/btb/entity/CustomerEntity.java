38cff047835452c655476bfb27578ca418bf620d
Mythili <revu.mythili@capgemini.com>
2025-03-13 09:39:11 +0530

    Customer entity class updated

--- BTBCustomerService/src/main/java/com/sprint/btb/entity/CustomerEntity.java
+++ BTBCustomerService/src/main/java/com/sprint/btb/entity/CustomerEntity.java
@@ -1,55 +1,43 @@
 package com.sprint.btb.entity;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "customers")
+@Table(name = "customer")
 public class CustomerEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "customer_id")
-	private int customerId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="customer_id")
+    private int customerId;
 
-	@Column(name = "name", nullable = false)
-	private String custName;
+    @Column(name = "name")
+    private String name;
 
-	@Column(name = "email", nullable = false, unique = true)
-	private String custEmail;
+    @Column(name = "email")
+    private String email;
 
-	@Column(name = "phone", nullable = false, unique = true)
-	private String custPhone;
+    @Column(name = "phone")
+    private String phone;
 
-	@Column(name = "address_id")
-	private Long addressId;
+    @Column(name = "address_id")
+    private int addressId;
+    
+    @Column(name = "password")
+    private String password;
 
-	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<PaymentEntity> payments;
+    public CustomerEntity() {
+        super();
+    }
 
-	public CustomerEntity() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public CustomerEntity(int customerId, String custName, String custEmail, String custPhone, Long addressId,
-			List<PaymentEntity> payments) {
+	public CustomerEntity(int customerId, String name, String email, String phone, int addressId, String password) {
 		super();
 		this.customerId = customerId;
-		this.custName = custName;
-		this.custEmail = custEmail;
-		this.custPhone = custPhone;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
 		this.addressId = addressId;
-		this.payments = payments;
+		this.password = password;
 	}
 
 	public int getCustomerId() {
@@ -60,50 +48,57 @@ public class CustomerEntity {
 		this.customerId = customerId;
 	}
 
-	public String getCustName() {
-		return custName;
+	public String getName() {
+		return name;
 	}
 
-	public void setCustName(String custName) {
-		this.custName = custName;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public String getCustEmail() {
-		return custEmail;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setCustEmail(String custEmail) {
-		this.custEmail = custEmail;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
-	public String getCustPhone() {
-		return custPhone;
+	public String getPhone() {
+		return phone;
 	}
 
-	public void setCustPhone(String custPhone) {
-		this.custPhone = custPhone;
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
-	public Long getAddressId() {
+	public int getAddressId() {
 		return addressId;
 	}
 
-	public void setAddressId(Long addressId) {
+	public void setAddressId(int addressId) {
 		this.addressId = addressId;
 	}
 
-	public List<PaymentEntity> getPayments() {
-		return payments;
+	public String getPassword() {
+		return password;
 	}
 
-	public void setPayments(List<PaymentEntity> payments) {
-		this.payments = payments;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	@Override
 	public String toString() {
-		return "CustomerEntity [customerId=" + customerId + ", custName=" + custName + ", custEmail=" + custEmail
-				+ ", custPhone=" + custPhone + ", addressId=" + addressId + ", payments=" + payments + "]";
+		return "CustomerEntity [customerId=" + customerId + ", name=" + name + ", email=" + email + ", phone=" + phone
+				+ ", addressId=" + addressId + ", password=" + password + "]";
 	}
 
+    
+	
+
+    
+    
+    
 }
+
